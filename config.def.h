f7b937ba5edc54e6e0cc430fd1c275584f4da0e7
rafasaurus <rafa.grigorian@gmail.com>
2019-12-07 13:34:51 +0400

    Update config

--- config.def.h
+++ config.def.h
@@ -5,21 +5,21 @@ static const unsigned int borderpx  = 7;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "LeagueMono-Bold:size=12" };
+static const char dmenufont[]       = "LeagueMono-Bold:size=14";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_cyan[]        = "#002277";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_gray4, col_cyan,  "#AFD700"  },
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "1:term", "2:web", "3:chat", "4:office", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
 	/* xprop(1):
