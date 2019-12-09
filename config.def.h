da01e6459be7352793b56c8a0e5f1de8aa5cec2a
rafasaurus <rafa.grigorian@gmail.com>
2019-12-09 16:00:21 +0400

    Update config

--- config.def.h
+++ config.def.h
@@ -3,10 +3,10 @@
 /* appearance */
 static const unsigned int borderpx  = 7;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappih    = 32;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 32;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 32;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 32;       /* vert outer gap between windows and screen edge */
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -16,13 +16,14 @@ static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_gray5[]       = "#28282A";
+static const char col_cyan[]        = "#00ECF9";
 static const unsigned int baralpha = 0xd0;
 static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  "#AFD700"  },
+	[SchemeSel]  = { col_gray5, col_cyan,  "#AFD700"  },
 };
 static const unsigned int alphas[][3]      = {
 	/*               fg      bg        border     */
