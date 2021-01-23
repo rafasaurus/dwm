97c134f8c9ff814fcfe198f191682c877fef8ab7
rafasaurus <rafa.grigorian@gmail.com>
2021-01-23 13:28:06 +0400

    Fix x coordinate calculation in buttonpress.

--- dwm.c
+++ dwm.c
@@ -582,7 +582,7 @@ buttonpress(XEvent *e)
 		} else if (ev->x < x + blw)
 			click = ClkLtSymbol;
 		/* 2px right padding */
-		else if (ev->x > selmon->ww - TEXTW(stext) + lrpad - 2)
+		else if (ev->x > selmon->ww - (int)TEXTW(stext) + lrpad - 2)
 			click = ClkStatusText;
 		else {
 			x += blw;
