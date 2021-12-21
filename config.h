734e5f3f31cd27c97e7fcaf92b6f156288d1dadb
rafasaurus <rafaelkgrigoryan@gmail.com>
2021-12-21 13:22:49 +0400

    Add, macos cursor, update gap sizes

--- config.h
+++ config.h
@@ -3,10 +3,10 @@
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 20;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 20;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 20;       /* vert outer gap between windows and screen edge */
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int swallowfloating    = 1;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
@@ -64,6 +64,7 @@ static const Rule rules[] = {
     { "jetbrains-studio",       NULL,       NULL,                   1<<4,         0,                0,           -1 },
     { "rhythmbox",              NULL,       NULL,                   0,            1,                0,           -1 },
     { "Rhythmbox",              NULL,       NULL,                   0,            1,                1,           -1 },
+    { "Mavproxy",               NULL,       NULL,                   1<<3,         0,                1,           -1 },
     /* class                    instance    title                   tags mask     0,                isfloating  isterminal  noswallow  monitor */
     { "vlc",                    NULL,       NULL,                   1<<4,         0,                0,          0,          -1,        -1 },
     { "sxiv",                   NULL,       NULL,                   0,            0,                0,          0,          -1,        -1 },
@@ -72,8 +73,10 @@ static const Rule rules[] = {
     { "Chromium",               NULL,       NULL,                   1<<1,         0,                0,          -1,          1,        -1 },
     { "Brave-browser",          NULL,       NULL,                   1<<1,         0,                0,          -1,          -1,        -1 },
     { "QGroundControl",         NULL,       NULL,                   1<<3,         0,                0,          -1,          -1,        -1 },
+    { "XTerm",                  NULL,       NULL,                   1<<4,         0,                0,          -1,          -1,        -1 },
     { "Cpupower-gui",           NULL,       NULL,                   0,            1,                1,          -1,          -1,        -1 },
     { NULL,                     "popup",    NULL,                   0,            1,                1,          1,          -1,        -1 },
+    { "wmbubble",               NULL,       NULL,                   0,            1,                1,          1,          -1,        -1 },
 };
 
 /* layout(s) */
