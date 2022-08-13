1254dd4154a23844a4ee186d3c8c14990e53e8bb
rafasaurus <rafaelkgrigoryan@gmail.com>
2022-08-13 14:55:41 +0400

    manage: propertynotify: Reduce cost of unused size hints
    This patch defers all size hint calculations until they are actually
    needed, drastically reducing the number of calls to updatesizehints(),
    which can be expensive when called repeatedly (as it currently is during
    resizes).
    
    In my unscientific testing this reduces calls to updatesizehints() by
    over 90% during a typical work session. There are no functional changes
    for users other than an increase in responsiveness after resizes and
    a reduction in CPU time.
    
    In slower environments or X servers, this patch also offers an
    improvement in responsiveness that is often tangible after resizing a
    client that changes hints during resizes.

--- dwm.c
+++ dwm.c
@@ -111,7 +111,7 @@ struct Client {
 	int x, y, w, h;
 	int sfx, sfy, sfw, sfh; /* stored float geometry, used on mode revert */
 	int oldx, oldy, oldw, oldh;
-	int basew, baseh, incw, inch, maxw, maxh, minw, minh;
+	int basew, baseh, incw, inch, maxw, maxh, minw, minh, hintsvalid;
 	int bw, oldbw;
 	unsigned int tags;
     int isfixed, iscentered, isfloating, isurgent, neverfocus, oldstate, isfullscreen, isterminal, noswallow;
@@ -439,6 +439,8 @@ applysizehints(Client *c, int *x, int *y, int *w, int *h, int interact)
 	if (*w < bh)
 		*w = bh;
 	if (resizehints || c->isfloating || !c->mon->lt[c->mon->sellt]->arrange) {
+		if (!c->hintsvalid)
+			updatesizehints(c);
 		/* see last two sentences in ICCCM 4.1.2.3 */
 		baseismin = c->basew == c->minw && c->baseh == c->minh;
 		if (!baseismin) { /* temporarily remove base dimensions */
@@ -1426,7 +1428,6 @@ manage(Window w, XWindowAttributes *wa)
 	XSetWindowBorder(dpy, w, scheme[SchemeNorm][ColBorder].pixel);
 	configure(c); /* propagates border_width, if size doesn't change */
 	updatewindowtype(c);
-	updatesizehints(c);
 	updatewmhints(c);
 	c->sfx = c->x;
 	c->sfy = c->y;
@@ -1627,7 +1628,7 @@ propertynotify(XEvent *e)
 				arrange(c->mon);
 			break;
 		case XA_WM_NORMAL_HINTS:
-			updatesizehints(c);
+			c->hintsvalid = 0;
 			break;
 		case XA_WM_HINTS:
 			updatewmhints(c);
@@ -2522,6 +2523,7 @@ updatesizehints(Client *c)
 	} else
 		c->maxa = c->mina = 0.0;
 	c->isfixed = (c->maxw && c->maxh && c->maxw == c->minw && c->maxh == c->minh);
+	c->hintsvalid = 1;
 }
 
 void
